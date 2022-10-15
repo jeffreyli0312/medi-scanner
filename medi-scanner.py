786e922c0c4678725d95c793460661008dda0484
SemiCollin <ckwan26.email@gmail.com>
2022-10-15 11:39:06 -0400

    built model

--- medi-scanner.py
+++ medi-scanner.py
@@ -76,11 +76,16 @@ print(class_names)
 #     plt.axis("off")
 # plt.show()
 
-# num_classes = 3
-# model = tf.keras.Sequential([
-#     tf.keras.layers.Rescaling(1./255),
-#     tf.keras.layers.Conv2D(32 , 3 , activation='relu'),
-#     tf.keras.layers.MaxPooling2D(),
-#     tf.keras.layers.Conv2D(32 , 3 , activation='relu'),
-
-# ])
+num_classes = 3
+model = tf.keras.Sequential([
+    tf.keras.layers.Rescaling(1./255),
+    tf.keras.layers.Conv2D(32 , 3 , activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(32 , 3 , activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(32 , 3 , activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128 , activation='relu'),
+    tf.keras.layers.Dense(num_classes)
+])
