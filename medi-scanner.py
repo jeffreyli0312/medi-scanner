2cda91e9db7a75091392db43ed0db62d9fe17988
SemiCollin <ckwan26.email@gmail.com>
2022-10-15 10:54:24 -0400

    renamed files to all be PNG for image processing created fucntioning datasets for ML matplotlib works to display images with given labels

--- medi-scanner.py
+++ medi-scanner.py
@@ -1,150 +1,3 @@
-import numpy as np
-import cv2
-import tensorflow
-from tensorflow import keras
-
-# from sklearn.datasets import make_regression, make_classification, make_blobs
-# import pandas as pd
-# import matplotlib.pyplot as plt
-
-# # TensorFlow and tf.keras
-# import tensorflow.compat.v1 as tf
-
-# from tensorflow import keras
-
-# # Helper libraries
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-# print(tf.__version__)
-
-# fashion_mnist = keras.datasets.fashion_mnist
-
-# (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-import cv2
-from tqdm import tqdm
-
-# Importing all necessary libraries
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
-from keras import backend as K
-
-img_width, img_height = 224, 224
-
-train_data_dir = 'C:/Users/Owner/Documents/Jeffrey/Hack The Valley/medi-scanner/Data/Train'
-validation_data_dir = 'C:/Users/Owner/Documents/Jeffrey/Hack The Valley/medi-scanner/Data/Validation'
-nb_train_samples =36
-nb_validation_samples = 7
-epochs = 10
-batch_size = 20
-
-if K.image_data_format() == 'channels_first':
-    input_shape = (3, img_width, img_height)
-else:
-    input_shape = (img_width, img_height, 3)
-
-model = Sequential()
-model.add(Conv2D(32, (2, 2), input_shape=input_shape))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-  
-model.add(Conv2D(32, (2, 2)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-  
-model.add(Conv2D(64, (2, 2)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-  
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
-
-
-train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
-  
-test_datagen = ImageDataGenerator(rescale=1. / 255)
-  
-train_generator = train_datagen.flow_from_directory(
-    train_data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
-    class_mode='binary')
-  
-validation_generator = test_datagen.flow_from_directory(
-    validation_data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
-    class_mode='binary')
-  
-model.fit_generator(
-    train_generator,
-    steps_per_epoch=nb_train_samples // batch_size,
-    epochs=epochs,
-    validation_data=validation_generator,
-    validation_steps=nb_validation_samples // batch_size)
-
-
-model.save_weights('model_saved.h5')
-
-from keras.models import load_model
-from keras.preprocessing.image import load_img ### HERE
-
-from keras.preprocessing.image import img_to_array
-from keras.applications.vgg16 import preprocess_input
-from keras.applications.vgg16 import decode_predictions
-from keras.applications.vgg16 import VGG16
-import numpy as np
-  
-from keras.models import load_model
-  
-model = load_model('model_saved.h5')
-  
-image = load_img('C:/Users/Owner/Documents/Jeffrey/Hack The Valley/medi-scanner/Data/Testing Image.jpg', target_size=(224, 224))
-img = np.array(image)
-img = img / 255.0
-img = img.reshape(1,224,224,3)
-label = model.predict(img)
-print("Predicted Class (0 - First Degree Burn , 1- Healthy Skin): ", label[0][0])
-
-# DATADIR = "C:/Users/Owner/Documents/Jeffrey/Hack The Valley/medi-scanner/Data"
-
-# CATEGORIES = ["First Degree", "Second Degree", "Third Degree", "Healthy Skin"]
-
-# for category in CATEGORIES: # do 1st, 2nd, 3rd
-#     path = os.path.join(DATADIR,category) 
-#     for img in os.listdir(path):  # iterate over each image per dogs and cats
-#         img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
-#         plt.imshow(img_array, cmap='gray')  # graph it
-#         plt.show()  # display!
-
-#         IMG_SIZE = 200
-
-#         new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-#         plt.imshow(new_array, cmap='gray')
-#         plt.show()
-
-#         break
-#     break
-
-
 # def main():
 
 #     # ==============================================================
@@ -166,3 +19,58 @@ print("Predicted Class (0 - First Degree Burn , 1- Healthy Skin): ", label[0][0]
 
 # if __name__ == '__main__':
 #     main()
+
+import tensorflow as tf
+import numpy as np
+import os
+import PIL
+import PIL.Image
+import pathlib
+
+import matplotlib.pyplot as plt
+import cv2
+import logging
+logger = tf.get_logger()
+logger.setLevel(logging.ERROR)
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+base_dir = os.path.dirname("C:\medi-scanner\dataset")
+base_dir = os.path.join(base_dir , "dataset")
+
+base_dir = pathlib.Path(base_dir)
+image_count = len(list(base_dir.glob("*/*.png")))
+print(image_count)
+
+batch_size = 16
+img_height , img_width = 180 , 180
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    base_dir,
+    validation_split = 0.2,
+    subset = "training",
+    seed = 123,
+    image_size = (img_height , img_width),
+    batch_size = batch_size
+)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    base_dir,
+    validation_split = 0.2,
+    subset = "validation",
+    seed = 123,
+    image_size = (img_height , img_width),
+    batch_size = batch_size
+)
+
+class_names = train_ds.class_names
+print(class_names)
+
+# plt.figure(figsize=(10, 10))
+# for images, labels in train_ds.take(1):
+#   for i in range(9):
+#     ax = plt.subplot(3, 3, i + 1)
+#     plt.imshow(images[i].numpy().astype("uint8"))
+#     plt.title(class_names[labels[i]])
+#     plt.axis("off")
