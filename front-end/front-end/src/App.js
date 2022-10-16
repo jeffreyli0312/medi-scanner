f28fa70fd3b4f12fd1a400e1ea408bd730a87f2f
Sung-En-Huang <s385huan@uwaterloo.ca>
2022-10-16 01:47:26 -0400

    Added upload function and console log upload percentage

--- front-end/front-end/src/App.js
+++ front-end/front-end/src/App.js
@@ -1,20 +1,58 @@
+// eslint-disable-next-line
+import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import InjuryList from "./InjuryList";
 import axios from "axios";
-import React, { Component } from "react";
 
+class App extends Component {
+  state = {
+    selectedFile: null,
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+  fileSelectedHandler = (event) => {
+    this.setState({});
+  };
+
+  fileUploadHandler = () => {
+    const fd = new FormData();
+    fd.append(
+      "image",
+      this.state.selectedFile,
+      this.state.selectedFile.className
+    );
+    axios
+      .post("", fd, {
+        onUploadProgress: (progressEvent) =>
+          console.log(
+            "Upload Progress: " +
+              Math.round((progressEvent.loaded / progressEvent.total) * 100) +
+              "%"
+          ),
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  };
+  // need api endpoint to store files in a storage or db
+  //   .then(res =>{
+  //     console.log(res);
+  //   }
+  // });
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Home />
+        </div>
+        <input type="file" onChange={this.fileSelectedHandler} />
+        <button onClick={this.fileUploadHandler}>Upload</button>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- front-end/front-end/src/InjuryList.js
+++ front-end/front-end/src/InjuryList.js
@@ -1,15 +1,12 @@
-const InjuryList = ({ injury, title }) => {
-  // const injury = props.injury;
-  // const title = props.title;
-  // console.log(injury);
-
+import React from "react";
+const InjuryList = ({ injury, title, handleDelete }) => {
   return (
     <div className="injury-list">
       <h2>{title}</h2>
       {injury.map((injury) => (
         <div className="injury-preview" key={injury.id}>
           <h2>{injury.title}</h2>
-          <p>Powered by {injury.author}</p>
+          <button onClick={() => handleDelete(injury.id)}>delete injury</button>
         </div>
       ))}
     </div>
